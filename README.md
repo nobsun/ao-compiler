2853e772270ab218bc9addf1ac629e886587f896
Nobuo Yamashita <nobsun@sampou.org>
2026-07-04 11:33:02 +0900

    start project

---
+++ README.md
@@ -0,0 +1,17 @@
+# 大堀淳『コンパイラ — 原理と構造』を読む
+
+## 教科書について
+
+- Amazon（with アフィリエイト）：
+    - [コンパイラ — 原理と構造（単行本）](https://amzn.to/3j5ZwI6)
+    - [コンパイラ — 原理と構造（Kindle版）](https://amzn.to/4xabeFW)
+- サポートサイト：
+    - [コンパイラ ー 原理と構造](https://atsushiohori.github.io/ja/texts/compiler/)
+- ビデオ教材（著者自身による）：
+    - [コンパイラ—原理と構造—](https://youtube.com/playlist?list=PLp1bKoc7XHdQwbHaUzMryG6WpnN4JdR6P&si=drmjH2SADzNY6Dkj)
+
+## コード
+
+教科書の主なコード（SML♯）は、サポートサイトから辿れるGitHubリポジトリで提供されています。
+
+ここでは、[Haskell](https://haskell.org)で実装します。もちろん、非公認なものです。
---
+++ app/turing/Main.hs
@@ -0,0 +1,20 @@
+-- # Main 雛形
+--
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE GHC2024 #-}
+{-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE LexicalNegation #-}
+{-# LANGUAGE LambdaCase, MultiWayIf #-}
+{-# LANGUAGE NPlusKPatterns #-}
+{-# LANGUAGE DataKinds, PolyKinds, NoStarIsType, TypeFamilyDependencies #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedRecordDot, NoFieldSelectors, DuplicateRecordFields #-}
+module Main 
+    ( main
+    ) where
+
+main :: IO ()
+main = do
+    { return ()
+    }
