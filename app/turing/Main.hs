a6de1cf664af080080fc7d4d9efb2eb5e6477c7c
Nobuo Yamashita <nobsun@sampou.org>
2026-07-13 15:16:32 +0900

    Ex.2.2

--- app/turing/Main.hs
+++ app/turing/Main.hs
@@ -20,9 +20,18 @@ import Language.Turing.Eval
 main :: IO ()
 main = print (tp0, eval pAddOne tp0)
     >> print (tp1, eval pAddOne tp1)
+    >> putStrLn "-- ex 2.2 --"
+    >> print (tp0', eval pAddOne' tp0')
+    >> print (tp1', eval pAddOne' tp1')
 
 tp0 :: Tape
 tp0 = ([I, I, I], I, [])
 
 tp1 :: Tape
 tp1 = ([I, O, I, I], I, [])
+
+tp0' :: Tape
+tp0' = ([], B, [I,I,I,I])
+
+tp1' :: Tape
+tp1' = ([], B, [I,O,I,I,I])
